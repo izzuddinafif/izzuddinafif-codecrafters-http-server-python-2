6d738efee025813326fd0c8080bb73e4cceff797
afif <izzuddinafif@gmail.com>
2025-04-07 14:07:07 +0700

    stage IA4

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-import socket  # noqa: F401
-
+import socket
 
 def main():
     port = 4221
     with socket.create_server(("localhost", port), reuse_port=True) as server:
-        print(f"Afif's HTTP server started at {port}")  
+        print(f"Afif's HTTP server started at {port}")
         while True:
             conn, addr = server.accept()
-            if conn:
-                print(f"connection from {addr}")    
-            
+            with conn:
+                print(f"connected by {addr[0]} from port {addr[1]}")
+                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
             
 if __name__ == "__main__":
     main()
