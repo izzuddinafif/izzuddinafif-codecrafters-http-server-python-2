87cd550ced4b54ae03fe33e4f90c4f12424e3797
afif <izzuddinafif@gmail.com>
2025-04-07 13:28:00 +0700

    stage AT4

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
-
-
+    port = 4221
+    with socket.create_server(("localhost", port), reuse_port=True) as server:
+        print(f"Afif's HTTP server started at {port}")  
+        while True:
+            conn, addr = server.accept()
+            if conn:
+                print(f"connection from {addr}")    
+            
+            
 if __name__ == "__main__":
     main()
