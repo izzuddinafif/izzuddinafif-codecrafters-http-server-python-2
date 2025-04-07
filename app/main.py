506bdec8787bec18fc6c3dd6662d04c114ae4f57
afif <izzuddinafif@gmail.com>
2025-04-07 15:28:04 +0700

    stage IH0

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def main():
     port = 4221
     with socket.create_server(("localhost", port), reuse_port=True) as server:
@@ -8,8 +9,21 @@ def main():
             conn, addr = server.accept()
             with conn:
                 print(f"connected by {addr[0]} from port {addr[1]}")
-                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+                while True:
+                    bytes_data = conn.recv(1024)
+                    data = str(bytes_data, 'utf-8')
+                    if not data:
+                        break
+                    splitted_data = data.split(' ')
+                    path = splitted_data[1]
+                    handlePath(conn, path)
 
-            
+def handlePath(conn, path):
+    if path != '/':
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    else:
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")            
+        
+        
 if __name__ == "__main__":
     main()
