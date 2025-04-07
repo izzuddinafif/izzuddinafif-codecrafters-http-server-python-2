8c019b5288f1801d40e39733c7a1382d8337a8dc
afif <izzuddinafif@gmail.com>
2025-04-07 16:59:56 +0700

    CN2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
 import socket
 
+# HTTP Respond Messages
+OK = b"HTTP/1.1 200 OK\r\n\r\n"
+NOTFOUND = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+def build_response(body, status="200 OK", content_type="text/plain"):
+    body_bytes = body.encode()
+    resp = [
+        f"HTTP/1.1 {status}",
+        f"Content-Type: {content_type}",
+        f"Content-Length: {len(body_bytes)}",
+        "",
+        "",
+    ]
+    
+    return "\r\n".join(resp).encode() + body_bytes
 
 def main():
     port = 4221
@@ -14,16 +29,18 @@ def main():
                     data = str(bytes_data, 'utf-8')
                     if not data:
                         break
-                    splitted_data = data.split(' ')
-                    path = splitted_data[1]
+                    request_line = data.splitlines()[0]
+                    method, path, version = request_line.split(' ')
                     handlePath(conn, path)
 
 def handlePath(conn, path):
-    if path != '/':
-        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    p = path.split('/')
+    if p[1] == '':
+        conn.sendall(OK)
+    if p[1] == 'echo' and len(p) > 2:
+        conn.sendall(build_response(p[2], "200 OK", "text/plain"))
     else:
-        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")            
-        
+        conn.sendall(NOTFOUND)      
         
 if __name__ == "__main__":
     main()
