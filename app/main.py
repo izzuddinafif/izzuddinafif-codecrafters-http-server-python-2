ae7b189ff978ebc23e967b49c8d5ac6194f54fd5
afif <izzuddinafif@gmail.com>
2025-04-11 16:04:40 +0700

    EJ5

--- app/main.py
+++ app/main.py
@@ -1,26 +1,33 @@
 import socket
+import logging
+import threading
 
-# HTTP Respond Messages
 NOTFOUND = b'HTTP/1.1 404 Not Found\r\n\r\n'
 
 def main():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
     port = 4221
     with socket.create_server(('localhost', port), reuse_port=True) as server:
-        print(f"Afif's HTTP server started at {port}")
+        logging.info(f"Afif's HTTP server started at {port}")
         while True:
             conn, addr = server.accept()
-            with conn:
-                print(f'connected by {addr[0]} from port {addr[1]}')
-                while True:
-                    bytes_data = conn.recv(1024)
-                    data = str(bytes_data, 'utf-8')
-                    if not data:
-                        break
-                    lines = data.splitlines()
-                    method, path, version = lines[0].split(' ')
-                    user_agent_lines = [ua for ua in lines if ua.startswith("User-Agent:")]
-                    user_agent = user_agent_lines[0].split(' ', 1)[1] if user_agent_lines else "Unknown"
-                    handle_request(conn, path, user_agent)
+            t = threading.Thread(target=handle_client, args=(conn, addr))
+            t.start()
+            
+def handle_client(conn, addr):
+    with conn:
+        logging.info(f'connected by {addr[0]} from port {addr[1]}')
+        while True:
+            bytes_data = conn.recv(1024)
+            data = str(bytes_data, 'utf-8')
+            if not data:
+                break
+            lines = data.splitlines()
+            method, path, version = lines[0].split(' ')
+            user_agent_lines = [ua for ua in lines if ua.startswith("User-Agent:")]
+            user_agent = user_agent_lines[0].split(' ', 1)[1] if user_agent_lines else "Unknown"
+            handle_request(conn, path, user_agent)
 
 def build_response(body, status, content_type):
     body_bytes = body.encode()
@@ -39,14 +46,19 @@ def build_response(body, status, content_type):
 def handle_request(conn, path, user_agent):
     p = path.split('/') 
     if p[1] == '':
-        print(build_response('', '200 OK', ''))
-        conn.sendall(build_response('', '200 OK', ''))
+        resp = build_response('', '200 OK', '')
+        logging.info(resp)
+        conn.sendall(resp)
     elif p[1] == 'echo' and len(p) > 2:
-        print(build_response(p[2], '200 OK', 'text/plain'))
-        conn.sendall(build_response(p[2], '200 OK', 'text/plain'))
+        resp = build_response(p[2], '200 OK', 'text/plain')
+        logging.info(resp)
+        conn.sendall(resp)
     elif p[1] == 'user-agent':
-        conn.sendall(build_response(user_agent, '200 OK', 'text/plain'))
+        resp = build_response(user_agent, '200 OK', 'text/plain')
+        logging.info(resp)
+        conn.sendall(resp)
     else:
+        logging.info(NOTFOUND)
         conn.sendall(NOTFOUND)
         
 if __name__ == '__main__':
